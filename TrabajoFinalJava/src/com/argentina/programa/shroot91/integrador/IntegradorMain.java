6ebc9383180f313649a80e12f05f6e0950cf4259
Llugdar Emilio Said <93954241+EmilioSaidLlugdar@users.noreply.github.com>
2023-04-13 21:19:51 -0300

    testing

--- TrabajoFinalJava/src/com/argentina/programa/shroot91/integrador/IntegradorMain.java
+++ TrabajoFinalJava/src/com/argentina/programa/shroot91/integrador/IntegradorMain.java
@@ -39,6 +39,8 @@ public class IntegradorMain {
 
             System.out.println("======================================");
             System.out.println();
+
+
            _leerArchivoPronosticoYGenerarObjetos(pronosticos);
             calcularPuntaje();
 
